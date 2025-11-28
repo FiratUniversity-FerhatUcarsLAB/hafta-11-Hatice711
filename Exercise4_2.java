25506915931d4125bbb89dedd0d6d485fa3043c5
Hatice711 <h438406@gmail.com>
2025-11-28 10:49:31 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -11,22 +11,32 @@ public class Exercise4_2 {
 
     // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
 
-    public static void main(String[] args) {
-        zippo("rattle", 13); //1
+    public static void main(String[] args) {//1
+        zippo("rattle", 13); //2
+    
     }
 
-    public static void baffle(String blimp) {
-        System.out.println(blimp); //4
-        zippo("ping", -5); //5
+    public static void baffle(String blimp) {//7
+        System.out.println(blimp); //8
+        zippo("ping", -5); //9
     }
 
-    public static void zippo(String quince, int flag) {
-        if (flag < 0) { //6
-            System.out.println(quince + " zoop"); //7
-        } else {
-            System.out.println("ik"); //2
-            baffle(quince); //3
-            System.out.println("boo-wa-ha-ha"); //8
+    public static void zippo(String quince, int flag) {//3,10
+        if (flag < 0) { //11
+            System.out.println(quince + " zoop"); //12
+        } else {//4
+            System.out.println("ik"); //5
+            baffle(quince); //6
+            System.out.println("boo-wa-ha-ha"); //13
         }
     }
 }
+
+/** Soruların Cevapları
+2. quince
+3.  ik
+    rattle
+    ping zoop
+    boo-wa-ha-ha
+4.  Sadece 24.satır iki kere çalışmıştır
+    */
