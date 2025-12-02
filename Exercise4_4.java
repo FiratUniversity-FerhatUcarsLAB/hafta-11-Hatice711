3b4ef7348c6aae7472ccf2e537face7484d89f8e
Hatice711 <h438406@gmail.com>
2025-12-02 10:21:28 +0300

    Add explanations for method calls in Exercise4_4
    Added comments explaining method calls and their behavior.

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -1,3 +1,11 @@
+/**
+* Ad Soyad : Hatice Kırmızgül
+* Öğrenci No : 250541102
+* Açıklama : Java da methot çağırma, ifade değerlendirmesi.
+* Tarih : 2.12.2025
+*/    
+
+
 public class Exercise4_4 {
 
     public static int getNumber() {
@@ -12,9 +20,18 @@ public class Exercise4_4 {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
         getNumber();
+        // CEVAP 1 :
+        // Return eden metodu çağırıp dönen methodu kullanmazsak 
+        // Java herhangi bir uyarı vermez.
+        // Metot normal şekilde çalışır, dönen değer kullanılmaz.
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
+        // CEVAP 2 :
+        // Burada void kullanamayız çünkü bir ifade değil.
+        // Java bir değeri olmayan ifadeyi başka işlemde kullanamaz.
+        // Derleme hatası olur.
+        // Çünkü sayHello() metotu void döndürür, yani bir değeri yoktur.
 
         // Cevaplarinizi yorum olarak ekleyin.
     }
